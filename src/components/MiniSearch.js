991ad8f20af5a6dea17d4018067ac87c334631d7
CHdev <codigo.turismointeractivo360@gmail.com>
2024-10-22 14:19:32 -0500

    Cambios más profundos en todos los componentes

--- src/components/MiniSearch.js
+++ src/components/MiniSearch.js
@@ -114,7 +114,7 @@ const MiniSearch = () => {
         initial={{ opacity: 0, y: -10 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.2 }}
-        className="text-3xl md:text-4xl font-bold ml-4"
+        className="text-2xl md:text-4xl font-bold ml-4"
       >
         {user?.username ? (
           <span className="dark:text-white text-[#f7bb17]">
